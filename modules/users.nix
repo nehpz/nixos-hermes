4f9dabd375b1565545e8c1d22f2889a19156cf02
nehpz <admin@rzp.one>
2026-04-19 18:41:55 -0700

    Add hermes user account
    Defines the hermes user account and configures its SSH authorized keys in the NixOS configuration.

--- modules/users.nix
+++ modules/users.nix
@@ -18,4 +18,10 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3neF+6qsDFb1pwr06mdW0mqMcxquAGNsjbGiG/Rj23"
     ];
   };
+  users.users.hermes = {
+    description = "Hermes user account";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0inarJ3Em+01Y22ahDmJkbhevhwuFFrWyIEl0CjkzE"
+    ];
+  };
 }
